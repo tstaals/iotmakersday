2cb33e5ca7bc416d7e0391bf58f66856ff3e3b9c
Tom Staals <tstaals@staalsonline.nl>
2017-05-21 22:23:21 +0200

    removed arduinoota from code timing issues with dht11

--- code/src/sensor.cpp
+++ code/src/sensor.cpp
@@ -3,24 +3,24 @@
 #include <ESP8266WebServer.h>     //Local WebServer used to serve the configuration portal
 #include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager WiFi Configuration Magic
 #include <ESP8266mDNS.h>
-#include <ArduinoOTA.h>
 #include <Adafruit_MQTT.h>
 #include <Adafruit_MQTT_Client.h>
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <DHT_U.h>
 
+#define S_DEBUG
 #define DHTPIN            D6        // Pin which is connected to the DHT sensor.
-#define PIRPIN            D4
-#define RESPIN            D3
-#define LDRPIN            A0
+#define PIRPIN            D2        // Pin for Passive Infrared Sensor
+#define RESPIN            D3        // Reset PIN for software reset
+#define LDRPIN            A0        // Pin for the LDR
 
 #define AIO_SERVER      "io.adafruit.com"
 #define AIO_SERVERPORT  1883
-#define AIO_USERNAME    "f1cyber"
-#define AIO_KEY         "3fbe669b986242eebd71a8d07ab7da28"
+#define AIO_USERNAME    ""
+#define AIO_KEY         ""
 
-#define DHTTYPE           DHT11     // DHT 22 (AM2302)
+#define DHTTYPE          DHT11     // DHT 22 (AM2302)
 
 int sensorValue;
 byte pirState;
@@ -29,14 +29,14 @@ byte oldPirState;
 DHT_Unified dht(DHTPIN, DHTTYPE);
 WiFiClient client;
 
-int delayTime =  10000;//300000;  //Wait 5 minutes before sending data to web
+int delayTime =  10000;   //300000;  //Wait 5 minutes before sending data to web
 int startDelay = 0;
 
 // Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
 Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
-Adafruit_MQTT_Publish temp = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/temps1");
-Adafruit_MQTT_Publish hum = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/hums1");
-Adafruit_MQTT_Publish ldr = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/lights1");
+Adafruit_MQTT_Publish temp = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/temps2");
+Adafruit_MQTT_Publish hum = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/hums2");
+Adafruit_MQTT_Publish ldr = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/lights2");
 
 void MQTT_connect() {
   int8_t ret;
@@ -65,7 +65,7 @@ void MQTT_connect() {
 }
 
 void motionChange() {
-    pirState = digitalRead(PIRPIN);
+     pirState = digitalRead(PIRPIN);
 }
 
 void resetNode() {
@@ -92,9 +92,7 @@ void setup()
   wifiManager.addParameter(&custom_mqtt_user);
   wifiManager.addParameter(&custom_mqtt_key);
 
-  wifiManager.autoConnect("AutoConnectAP");
-
-  //wifiManager.resetSettings();
+  wifiManager.autoConnect("AutoConnectJitske");
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(1500);
@@ -141,36 +139,11 @@ void setup()
   Serial.println("------------------------------------");
   #endif
 
-  ArduinoOTA.onStart([]() {
-  String type;
-  if (ArduinoOTA.getCommand() == U_FLASH)
-    type = "sketch";
-  else // U_SPIFFS
-    type = "filesystem";
 
-  // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-  Serial.println("Start updating " + type);
-});
-ArduinoOTA.onEnd([]() {
-  Serial.println("\nEnd");
-});
-ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-  Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-});
-ArduinoOTA.onError([](ota_error_t error) {
-  Serial.printf("Error[%u]: ", error);
-  if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-  else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-  else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-  else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-  else if (error == OTA_END_ERROR) Serial.println("End Failed");
-});
-ArduinoOTA.begin();
 }
 
 void loop()
 {
-  ArduinoOTA.handle();
 
   MQTT_connect();
 
@@ -183,15 +156,15 @@ void loop()
   }
   else {
     startDelay = millis();
+
     sensorValue = analogRead(A0); // read analog input pin 0
     ldr.publish(sensorValue);
-
+    delay(2000);
     // Delay between measurements.
     // Get temperature event and print its value.
     sensors_event_t event;
     dht.temperature().getEvent(&event);
     if (isnan(event.temperature)) {
-      Serial.println(event.temperature);
       Serial.println("Error reading temperature!");
     }
     else {
